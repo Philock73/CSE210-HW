5a06a844054af691ba22878578e465472858a90f
Philock73 <nathanbouck@byui.edu>
2026-04-22 14:59:56 -0600

    Setting up the program for prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What grade did you get?");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry but you failed");
+        }
     }
 }

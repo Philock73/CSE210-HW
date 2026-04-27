7e26ae872b87ac33471bc40716f71affdb79f819
Philock73 <nathanbouck@byui.edu>
2026-04-27 14:59:58 -0600

    Made the game be repeated ifthe player wants.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,36 +1,42 @@
 using System;
 using System.ComponentModel;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Random number = new Random();
-        int magic = number.Next(1,100);
-        int confused = 0;
-        int x = 1;
-        while (confused == 0)
-        {
-             Console.WriteLine("What is the magic number?");
-            string guess = Console.ReadLine();
-            int guessednumber = int.Parse(guess);
-            if (guessednumber == magic)
+        string fun;
+        do{
+            Random number = new Random();
+            int magic = number.Next(1,100);
+            int confused = 0;
+            int x = 1;
+            while (confused == 0)
             {
-                confused = 1;
+                 Console.WriteLine("What is the magic number?");
+                string guess = Console.ReadLine();
+                int guessednumber = int.Parse(guess);
+                if (guessednumber == magic)
+                {
+                    confused = 1;
+                }
+                else if (guessednumber > magic)
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                x++;
             }
-            else if (guessednumber > magic)
-            {
-                Console.WriteLine("Guess Lower");
-            }
-            else
-            {
-                Console.WriteLine("Guess Higher");
-            }
-            x++;
-        }
        
         
-        Console.WriteLine($"That's it! The magic number is:{magic}");
-        Console.WriteLine($"It took you {x} guesses");
+            Console.WriteLine($"That's it! The magic number is:{magic}");
+            Console.WriteLine($"It took you {x} guesses");
+            Console.WriteLine("Do you want to play again?");
+            fun = Console.ReadLine();
+        } while(fun == "yes");
     }
 }

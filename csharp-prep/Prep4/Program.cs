782cd57a41b94f0a0be51f7b73e4bf4475c236b6
Philock73 <nathanbouck@byui.edu>
2026-04-29 14:19:07 -0600

    Got the lists working

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Globalization;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int gift;
+        Console.WriteLine("Please enter a number, when finished enter 0");
+        do
+        {
+            Console.Write("Enter number: ");
+            gift = int.Parse(Console.ReadLine());
+            numbers.Add(gift);
+        }while(gift != 0);
+        Console.WriteLine("Here is your list:");
+        Console.WriteLine(numbers.Count);
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
     }
 }

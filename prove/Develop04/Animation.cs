ed0b21a0a2c64b1280312981caf6bb645a152aa8
Philock73 <nathanbouck@byui.edu>
2026-06-08 15:02:36 -0600

    Got animations working

---
+++ prove/Develop04/Animation.cs
@@ -0,0 +1,14 @@
+﻿class Animation
+{
+    
+    public void MakeAnimation()
+    {
+        string animationString = "+/-|/";
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write(animationString[i % animationString.Length]);
+            Thread.Sleep(250);
+            Console.Write("\b");
+        }
+    }
+}
